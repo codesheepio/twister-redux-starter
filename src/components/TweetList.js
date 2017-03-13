4147aed96e3e3131b37d4a7801934a20dbf4952e
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 02:19:22 +0700

    Calls fetchTweets on componentDidMount

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,14 +1,26 @@
 import React, { PropTypes } from 'react'
 import Tweet from './Tweet'
 
-const TweetList = ({ tweets }) => (
-  <div className="tweet-list">
-    { tweets.map(tweet => <Tweet key={tweet.id} {...tweet} />) }
-  </div>
-)
+class TweetList extends React.Component {
+  componentDidMount() {
+    const mockTweets = [
+      { id: 1, name: 'John Doe', username: 'jd', tweetText: 'Hello', timestamp: 1234 },
+      { id: 2, name: 'Alice Well', username: 'aw', tweetText: 'Love React', timestamp: 5678 },
+    ]
+    this.props.fetchTweetsSuccess(mockTweets)
+  }
+  render() {
+    return (
+      <div className="tweet-list">
+        {this.props.tweets.map(tweet => <Tweet key={tweet.id} {...tweet} />)}
+      </div>
+    )
+  }
+}
 
 TweetList.propTypes = {
   tweets: PropTypes.arrayOf(PropTypes.object),
+  fetchTweetsSuccess: PropTypes.func.isRequired,
 }
 
 TweetList.defaultProps = {
