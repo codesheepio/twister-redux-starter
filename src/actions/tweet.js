fdf8919626509e21acd921d1905b54ad136543ed
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 03:02:40 +0700

    Create fetch tweets thunk

--- src/actions/tweet.js
+++ src/actions/tweet.js
@@ -1,10 +1,27 @@
 import { FETCH_TWEETS_SUCCESS } from './types'
+import config from '../config'
 
 const fetchTweetsSuccess = tweets => ({
   type: FETCH_TWEETS_SUCCESS,
   payload: { tweets },
 })
 
+const { host, port } = config.api
+
+const fetchTweets = username => (dispatch) => {
+  const uri = `http://${host}:${port}/api/Tweets?filter={"where":{"username":"${username}"}}`
+
+  fetch(uri)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(tweets => dispatch(fetchTweetsSuccess(tweets)))
+  .catch(err => console.error(err))
+}
+
 export {
-  fetchTweetsSuccess,
+  fetchTweets,
 }
