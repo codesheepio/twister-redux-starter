eb7334d0223ee5865a6cfff845fc96e4d7514c17
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 03:12:27 +0700

    Apply thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import { AppContainer } from 'react-hot-loader'
 import './styles/custom.scss'
 import './styles/main.scss'
 import App from './components/App'
 import rootReducer from './reducers'
 
+/* eslint-disable no-underscore-dangle,comma-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   rootReducer,
-  // eslint-disable-next-line no-underscore-dangle,comma-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 )
+/* eslint-enable */
 
 const render = (Component) => {
   ReactDOM.render(
