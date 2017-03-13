6492e0011c0f091e5409f1461ef53d62f94f89ea
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 02:36:01 +0700

    Create store and pass down with Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader'
 import './styles/custom.scss'
 import './styles/main.scss'
 import App from './components/App'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Provider store={store}>
+        <Component />
+      </Provider>
     </AppContainer>,
     document.getElementById('react-root'))
 }
