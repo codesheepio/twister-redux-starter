a7f5a12c4be6cd804b2489788d910bcb2df2da0d
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 02:13:22 +0700

    Create FETCH_TWEET_SUCCESS action creator

---
+++ src/actions/tweet.js
@@ -0,0 +1,10 @@
+import { FETCH_TWEETS_SUCCESS } from './types'
+
+const fetchTweetsSuccess = tweets => ({
+  type: FETCH_TWEETS_SUCCESS,
+  payload: { tweets },
+})
+
+export {
+  fetchTweetsSuccess,
+}
