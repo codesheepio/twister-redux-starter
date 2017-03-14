4421490a8e5d629fa1406939e85ed91dedb56345
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 20:07:36 +0700

    Create login thunk action creator

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,5 @@
 import { AUTH_LOGIN_SUCCESS } from './types'
+import config from '../config'
 
 const loginSuccess = (username, name, token) => ({
   type: AUTH_LOGIN_SUCCESS,
@@ -8,3 +9,34 @@ const loginSuccess = (username, name, token) => ({
     token,
   },
 })
+
+const { host, port } = config.api
+
+const login = (username, password) => (dispatch) => {
+  const uri = `http://${host}:${port}/api/TwisterUsers/login`
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Content-type': 'application/json',
+    },
+    mode: 'cors',
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(authInfo => dispatch(loginSuccess(authInfo.username, authInfo.name, authInfo.token)))
+  .catch(err => console.error(err))
+}
+
+export {
+  login,
+}
