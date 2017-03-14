8e27c8dd54f04b32e187e30e5a6358fff73352a7
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 22:03:03 +0700

    Add route to LoginForm

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import MainLayout from '../layouts/MainLayout'
 import BodyContainer from './BodyContainer'
+import LoginForm from '../containers/LoginForm'
 
 const App = ({ history }) => (
   <ConnectedRouter history={history}>
     <MainLayout>
       <Switch>
         <Route exact path="/" component={BodyContainer} />
+        <Route path="/login" component={LoginForm} />
         <Route path="/:ownerUsername" component={BodyContainer} />
       </Switch>
     </MainLayout>
