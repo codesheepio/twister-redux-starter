b2714b1b10ffb1b100f7c884b29bcda3fd1581ce
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-14 20:11:47 +0700

    Attach auth reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import tweetsReducer from './tweets'
+import authReducer from './auth'
 
 const rootReducer = combineReducers({
   tweets: tweetsReducer,
+  auth: authReducer,
 })
 
 export default rootReducer
