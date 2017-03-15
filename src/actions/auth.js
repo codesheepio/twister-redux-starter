a4fcd3a737ab869e085eb7b7a477b707958d1059
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-15 13:09:49 +0700

    Create signup thunk action creator

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,8 @@
+import { push } from 'connected-react-router'
 import { AUTH_LOGIN_SUCCESS } from './types'
 import config from '../config'
+
+const { host, port } = config.api
 
 const loginSuccess = (username, name, token) => ({
   type: AUTH_LOGIN_SUCCESS,
@@ -9,8 +12,6 @@ const loginSuccess = (username, name, token) => ({
     token,
   },
 })
-
-const { host, port } = config.api
 
 const login = (username, password) => (dispatch) => {
   const uri = `http://${host}:${port}/api/TwisterUsers/login`
@@ -37,6 +38,34 @@ const login = (username, password) => (dispatch) => {
   .catch(err => console.error(err))
 }
 
+const signup = (username, name, email, password) => (dispatch) => {
+  const uri = `http://${host}:${port}/api/TwisterUsers`
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+    },
+    mode: 'cors',
+    body: JSON.stringify({
+      username,
+      name,
+      email,
+      password,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(() => dispatch(push('/')))
+  .catch(err => console.err(err))
+}
+
 export {
   login,
+  signup,
 }
