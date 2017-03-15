25757763ad12a1b3b89e621de8eafb0c7b92e37e
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-15 17:06:17 +0700

    Create profile reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import tweetsReducer from './tweets'
 import authReducer from './auth'
+import profileReducer from './profile'
 
 const rootReducer = combineReducers({
   tweets: tweetsReducer,
   auth: authReducer,
   form: formReducer,
+  profile: profileReducer,
 })
 
 export default rootReducer
