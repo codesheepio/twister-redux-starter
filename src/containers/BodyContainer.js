17b58f8ad080f7556697809fd5fb183fa86cdd44
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-15 22:07:00 +0700

    Show NewTweet component if it is the root domain

--- src/containers/BodyContainer.js
+++ src/containers/BodyContainer.js
@@ -1,9 +1,17 @@
 import { connect } from 'react-redux'
 import { push as redirect } from 'connected-react-router'
+import { matchPath } from 'react-router'
 import BodyContainer from '../components/BodyContainer'
 
-const mapStateToProps = state => ({
-  token: state.auth.token,
-})
+const mapStateToProps = (state) => {
+  const match = matchPath(state.router.location.pathname, {
+    path: '/:ownerUsername',
+  })
+
+  return {
+    token: state.auth.token,
+    enableTweet: !match,
+  }
+}
 
 export default connect(mapStateToProps, { redirect })(BodyContainer)
