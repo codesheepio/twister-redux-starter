631c1a5f3045d184470641de7783b5f5e964b683
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-15 22:02:40 +0700

    Create opstTweet thunk action creator

--- src/actions/tweet.js
+++ src/actions/tweet.js
@@ -1,4 +1,4 @@
-import { FETCH_TWEETS_SUCCESS } from './types'
+import { FETCH_TWEETS_SUCCESS, TWEET_POST_SUCCESS } from './types'
 import config from '../config'
 
 const fetchTweetsSuccess = tweets => ({
@@ -22,6 +22,46 @@ const fetchTweets = username => (dispatch) => {
   .catch(err => console.error(err))
 }
 
+const postTweetSuccess = (name, username, tweetText, timestamp) => ({
+  type: TWEET_POST_SUCCESS,
+  payload: {
+    name,
+    username,
+    tweetText,
+    timestamp,
+  },
+})
+
+const postTweet = (name, username, tweetText, token) => (dispatch) => {
+  const uri = `http://${host}:${port}/api/Tweets`
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      name,
+      username,
+      tweetText,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(data => dispatch(
+    // eslint-disable-next-line comma-dangle
+    postTweetSuccess(data.name, data.username, data.tweetText, data.timestamp)
+  ))
+  .catch(err => console.error(err))
+}
+
 export {
   fetchTweets,
+  postTweet,
 }
